0f229b2eba6ec33737dc5a4746fa7970eeee4c2d
Albert Bellonch Llargués <albert@itnig.net>
2013-10-08 22:02:34 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Melomerezco::Application.config.secret_key_base = '27df3cb00dd2b2af254386fcd21938d16e79b99c93dd5ca9751e2fe17edc505d5e6df04788b85e2255c6296608f6f0666bd3c9e39bfb782980bebe8e4ffe3530'
